3badd3aff6b6b3169f7a56cad7f8c41e1d13dd99
gold003dev <gold2003dev@hotmail.com>
2024-05-03 14:55:22 +0300

    fix clearing token issue

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -90,14 +90,6 @@ export const useAuthStore = defineStore("auth", () => {
   function verifyAuth() {
     if (JwtService.getToken()) {
       ApiService.setHeader();
-      ApiService.post("verify_token", { api_token: JwtService.getToken() })
-        .then(({ data }) => {
-          setAuth(data);
-        })
-        .catch(({ response }) => {
-          setError(response.data.errors);
-          purgeAuth();
-        });
     } else {
       purgeAuth();
     }
